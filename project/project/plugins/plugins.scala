2164d9c19f0cd980fb6f0bed22dba8b9bbf63a91
mystelynx <tomohiro.urakawa@gmail.com>
2012-07-15 09:49:17 +0900

    sbt-teamcity-test-reporting-plugin

---
+++ project/project/plugins/plugins.scala
@@ -0,0 +1,6 @@
+import sbt._
+
+object Plugins extends Build {
+  lazy val plugins = Project("plugins", file("."))
+  .dependsOn(uri("git://github.com/guardian/sbt-teamcity-test-reporting-plugin#1.2"))
+}
