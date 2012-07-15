5760892bfab46978df020aa0ae175a6f776aec96
mystelynx <tomohiro.urakawa@gmail.com>
2012-07-15 18:51:56 +0900

    #RAT-1 in progress tag: to be tested

---
+++ src/main/scala/com/github/mystelynx/rational/Rational.scala
@@ -0,0 +1,11 @@
+package com.github.mystelynx.rational
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: urakawa
+ * Date: 12/07/15
+ * Time: 18:49
+ * To change this template use File | Settings | File Templates.
+ */
+
+class Rational(n: Int, d: Int)
---
+++ src/test/scala/com/github/mystelynx/rational/RationalSpec.scala
@@ -0,0 +1,20 @@
+package com.github.mystelynx.rational
+
+import org.specs2.mutable.Specification
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: urakawa
+ * Date: 12/07/15
+ * Time: 18:47
+ * To change this template use File | Settings | File Templates.
+ */
+
+class RationalSpec extends Specification {
+
+  "Rational class" should {
+    "have 2 parameters to construct" in {
+      new Rational(1, 2) must not equals(null)
+    }
+  }
+}
